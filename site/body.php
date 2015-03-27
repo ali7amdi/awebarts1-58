18748a3ceef62d367c000f3212d50b5f3899cfdb
ali7amdi <ali_fergani2003@yahoo.com>
2015-03-28 01:07:59 +0300

    awebarts 1-58 PHP OOPMVC
    Source code lessons 1 :58 : PHP & MySQL OOP MVC - awebarts CMS

---
+++ site/body.php
@@ -0,0 +1,67 @@
+<div id="latestprojects">
+              <h1>Latest Projects:</h1>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            	<div class="project">
+           	    	<img src="<?php echo SITE;?>images/proj.png" width="140" height="100" />
+                    <h2>arabiangerman.com</h2>
+                    <p>
+AGEX was established in 2004 as exhibitions and conferences organizer in the Middle East...                    
+                    </p>
+                    <a href="">Read more »</a>
+                </div>
+            </div>
---
+++ site/page.php
@@ -0,0 +1,29 @@
+<?php
+// header.php
+include './header.php';
+
+// contents start
+echo '<div id="contents">';
+
+// sider bar
+include './sidebar.php';
+
+echo '<div id="conts">';
+
+echo '<div id="page">';
+
+/*
+ * 
+ * 
+ * 
+ */
+
+
+echo '</div>';
+echo '</div>';
+
+echo '</div>';
+
+// footer.php
+include './footer.php';
+?>
